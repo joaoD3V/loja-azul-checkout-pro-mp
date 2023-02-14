fe2267059080a54cb9000d3ee5e0923e32e7e411
Joao Silva <joao.devweb@gmail.com>
2023-02-14 09:47:01 -0300

    feat(feedback): add new field external reference

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -1,19 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-interface FeedbackBody {
-  Payment: string | string[];
-  Status: string | string[];
-  MerchantOrder: string | string[];
-}
-
-const handler = (req: NextApiRequest, res: NextApiResponse<FeedbackBody>) => {
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.query);
-  const { payment_id, status, merchant_order_id } = req.query;
+  const { payment_id, status, merchant_order_id, external_reference } =
+    req.query;
   res.status(200).json({
+    MerchantOrder: merchant_order_id!,
+    ExternalReference: external_reference!,
     Payment: payment_id!,
     Status: status!,
-    MerchantOrder: merchant_order_id!,
   });
 };
 
